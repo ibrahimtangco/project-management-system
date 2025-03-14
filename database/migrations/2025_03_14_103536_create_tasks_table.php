6c550021443f1792a6d1b6725eedc09181976ebe
Ibrahim Tangco <barangobong13@gmail.com>
2025-03-14 18:49:20 +0800

    Add models, controllers, and migrations files and migrated it

---
+++ database/migrations/2025_03_14_103536_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('user_id')->constrained();
+            $table->date('due_date');
+            $table->enum('priority_level', ['low', 'medium', 'high']);
+            $table->enum('status', ['todo', 'in-progress', 'done']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
